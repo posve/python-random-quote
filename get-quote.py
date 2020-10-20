1e129028b9fcbac37d0cdfeda21fb77850f0e2d8
Franklin Martínez Martínez <fdmartinez96@gmail.com>
2020-10-20 13:52:24 +0200

    Some more exercises with Python and text

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,19 @@ def funcion():
 
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  nueva_frase = "Another sentence for the program"
+  nueva_frase_2 = "A second sentence"
+  final_frase = "One final sentence"
+  quotes.append(nueva_frase)
+  quotes.append(nueva_frase_2)
+  quotes.append(final_frase)
+  for frase in quotes:
+    formateada = frase.rstrip()
+    print (formateada)
+  #f.close()
+  #last = 14
+  #rnd = random.randint(0, last)
+  #print(quotes[14], "\n", quotes[15], "\n", quotes[16], sep="")
 
 if __name__== "__main__":
   funcion()
